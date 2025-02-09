c0be7e0c9798ac1e381632f452a619e67c74db00
ruhsulin <alinaspahiu.a@gmail.com>
2025-02-09 15:20:12 +0100

    Added VillaController with Index method to retrive all villas.

---
+++ ProScape.Web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProScape.Infrastructure.Data;
+
+namespace ProScape.Web.Controllers;
+
+public class VillaController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public VillaController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var villas = _db.Villas.ToList();
+        return View(villas);
+    }
+}
