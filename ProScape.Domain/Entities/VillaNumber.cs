f384acb1d50e8d8e7cfa7c65418f3795422be8e5
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-15 03:23:22 +0100

    Added VillaNumber entity.

---
+++ ProScape.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProScape.Domain.Entities;
+
+public class VillaNumber
+{
+    // giving the unique id manually.
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Display(Name = "Villa Number")]
+    public int Villa_Number { get; set; }
+
+    [ForeignKey("Villa")]
+    public int VillaId { get; set; }
+
+    // Navigation property
+    [ValidateNever]
+    public Villa Villa { get; set; }
+
+    public string? SpecialDetails { get; set; }
+}
