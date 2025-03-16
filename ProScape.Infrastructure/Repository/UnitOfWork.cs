9f0233bef6fd7c5828b7c578c96c95f7280008c6
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-16 13:25:10 +0100

    Added impelemtation of UnitOfWork.

---
+++ ProScape.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using ProScape.Application.Common.Interfaces;
+using ProScape.Infrastructure.Data;
+
+namespace ProScape.Infrastructure.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _db;
+    public IVillaRepository Villa { get; private set; }
+
+    // Constructor
+    public UnitOfWork(ApplicationDbContext db)
+    {
+        _db = db;
+        Villa = new VillaRepository(_db);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+}
