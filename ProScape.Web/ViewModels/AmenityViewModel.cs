4ae096e59f68c98821d0c409f2ae12ca2e7369f0
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-22 18:31:14 +0100

    Added AmenityViewModel class.

---
+++ ProScape.Web/ViewModels/AmenityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProScape.Domain.Entities;
+
+namespace ProScape.Web.ViewModels;
+
+public class AmenityViewModel
+{
+    public Amenity? Amenity { get; set; }
+
+    [ValidateNever]
+    public IEnumerable<SelectListItem>? VillaList { get; set; }
+}
