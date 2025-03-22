444afed0660a6fc1a02e7d801b3ce9bb9048f3d2
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-22 18:30:25 +0100

    Added AmenityRepository.

---
+++ ProScape.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProScape.Application.Common.Interfaces;
+using ProScape.Domain.Entities;
+using ProScape.Infrastructure.Data;
+
+namespace ProScape.Infrastructure.Repository;
+
+public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+{
+    public readonly ApplicationDbContext _db;
+
+    public AmenityRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Update(Amenity entity)
+    {
+        _db.Amenities.Update(entity);
+    }
+}
