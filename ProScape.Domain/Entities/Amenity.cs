2a421369b391d0cf60ac867e202179c3bc897c45
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-22 18:29:57 +0100

    Added Amenity entity class.

---
+++ ProScape.Domain/Entities/Amenity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProScape.Domain.Entities;
+
+public class Amenity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    [ForeignKey("Villa")]
+    public int VillaId { get; set; }
+    [ValidateNever]
+    public Villa Villa { get; set; }
+}
