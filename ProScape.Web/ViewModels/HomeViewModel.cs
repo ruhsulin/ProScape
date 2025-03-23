d1c0c54516533e08927bbbf6f74d727dd05fea02
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-23 19:03:39 +0100

    Added HomeViewModel class.

---
+++ ProScape.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ProScape.Domain.Entities;
+
+namespace ProScape.Web.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<Villa>? VillaList { get; set; }
+    public DateOnly CheckInDate { get; set; }
+    public DateOnly? CheckOutDate { get; set; }
+    public int Nights { get; set; }
+}
