3c096a3c94fb8f4a5ff2894e6205a2d7147f6d98
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-28 21:41:27 +0100

    Added StaticDetails class to add constants.

---
+++ ProScape.Application/Common/Utility/StaticDetails.cs
@@ -0,0 +1,7 @@
+﻿namespace ProScape.Application.Common.Utility;
+
+public static class StaticDetails
+{
+    public const string Role_Admin = "Admin";
+    public const string Role_Client = "Client";
+}
