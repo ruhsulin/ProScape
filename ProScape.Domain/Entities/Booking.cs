736b1d4a4f2fb598a4ff0b3730a8e9ae22f07bdf
ruhsulin <alinaspahiu.a@gmail.com>
2025-04-13 18:15:16 +0200

    Added Booking class.

---
+++ ProScape.Domain/Entities/Booking.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProScape.Domain.Entities;
+
+public class Booking
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+    [ForeignKey("UserId")]
+    public ApplicationUser User { get; set; }
+
+
+    [Required]
+    public int VillaId { get; set; }
+    [ForeignKey("VillaId")]
+    public Villa Villa { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string Email { get; set; }
+    public string? Phone { get; set; }
+
+
+    [Required]
+    public double TotalCost { get; set; }
+    public int Nights { get; set; }
+    public string? Status { get; set; }
+
+
+    [Required]
+    public DateTime BookingDate { get; set; }
+    [Required]
+    public DateTime CheckInDate { get; set; }
+    [Required]
+    public DateTime CheckOutDate { get; set; }
+}
+
