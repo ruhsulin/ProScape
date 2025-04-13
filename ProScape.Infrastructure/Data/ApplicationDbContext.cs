647016f00604a970045609b8f2afad64cdc89eba
ruhsulin <alinaspahiu.a@gmail.com>
2025-04-13 18:16:19 +0200

    Added DbSet<Booking> in ApplicationDbContext.

--- ProScape.Infrastructure/Data/ApplicationDbContext.cs
+++ ProScape.Infrastructure/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<VillaNumber> VillaNumbers { get; set; }
     public DbSet<Amenity> Amenities { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
