9ca860c98588755e08cb47e939fb1886f0cd4ec9
ruhsulin <alinaspahiu.a@gmail.com>
2025-04-13 18:14:37 +0200

    Added Booking Status to StaticDetails.

--- ProScape.Application/Common/Utility/StaticDetails.cs
+++ ProScape.Application/Common/Utility/StaticDetails.cs
@@ -4,4 +4,11 @@ public static class StaticDetails
 {
     public const string Role_Admin = "Admin";
     public const string Role_Client = "Client";
+
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusCheckedIn = "CheckedIn";
+    public const string StatusCompleted = "Completed";
+    public const string StatusCancelled = "Cancelled";
+    public const string StatusRefunded = "Refunded";
 }
