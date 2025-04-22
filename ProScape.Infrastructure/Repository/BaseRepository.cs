7cbb72083cf6c4dbacac9c11e4c5ea47bc0dc5e5
ruhsulin <alinaspahiu.a@gmail.com>
2025-04-22 19:55:54 +0200

    Renamed Repository file to BaseRepository.

---
+++ ProScape.Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProScape.Application.Common.Interfaces;
+using ProScape.Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace ProScape.Infrastructure.Repository;
+
+public class BaseRepository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _db;
+    internal DbSet<T> dbSet;
+
+    public BaseRepository(ApplicationDbContext db)
+    {
+        _db = db;
+        dbSet = _db.Set<T>();
+    }
+
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+    }
+
+    public bool Any(Expression<Func<T, bool>> filter)
+    {
+        return dbSet.Any(filter);
+    }
+
+    public T Get(Expression<Func<T, bool>>? filter, string? includeProperties = null)
+    {
+        IQueryable<T> query = dbSet;
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            // Villa, VillaNumber -- case sensitive
+            foreach (var includeProp in includeProperties
+                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProp);
+            }
+        }
+        return query.FirstOrDefault();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+    {
+        IQueryable<T> query = dbSet;
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            // Villa, VillaNumber -- case sensitive
+            foreach (var includeProp in includeProperties
+                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProp);
+            }
+        }
+        return query.ToList();
+    }
+
+    public void Remove(T entity)
+    {
+        dbSet.Remove(entity);
+    }
+}
