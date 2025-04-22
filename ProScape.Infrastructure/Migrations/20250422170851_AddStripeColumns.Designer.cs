f95210a8d1489bef5e820e6b6a5290ee2ae6aff7
ruhsulin <alinaspahiu.a@gmail.com>
2025-04-22 19:54:43 +0200

    Added migrations for new booking columns

--- ProScape.Infrastructure/Migrations/20250422170851_AddStripeColumns.Designer.cs
+++ ProScape.Infrastructure/Migrations/20250422170851_AddStripeColumns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProScape.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using ProScape.Infrastructure.Data;
 namespace ProScape.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250422170851_AddStripeColumns")]
+    partial class AddStripeColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
